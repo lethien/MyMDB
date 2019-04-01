41b5ae14d12ebce4519254098c353653652f2497
TrevorFawcett <49043544+TrevorFawcett@users.noreply.github.com>
2019-04-01 00:03:48 -0700

    Update MoviesList.php

--- MoviesList.php
+++ MoviesList.php
@@ -13,9 +13,46 @@ Page::header();
 Page::page_head(true);
 
 // TODO: get movies list based on search term in POST
-$movies = array();
+$moviesList = array();
 
-Page::render_movie_list($movies);
+//Get every movie 
+$result = RestClient::call("GET",array());
+
+//De-serialize the result of the Rest call.
+$jmovies = json_decode($result);
+
+$allMovies = array();
+
+foreach ($jmovies as $m)  {
+    //Assemble a new movie class
+        $movie = new Movie();
+        $movie->setTitle($m->Title);
+		$movie->setPosterURL($m->Poster);
+		$movie->setSummary($m->PlotSummary);
+		$movie->setRuntime($m->Runtime);
+		$movie->setGenres($m->Genres);
+		$movie->setCrew($m->Crew);
+		$movie->setDirectors($m->Directors);
+		$movie->setAwards($m->Awards);
+		$movie->setCreatedBy($m->CreatedBy);
+    
+    $allMovies[] = $movie;
+}
+
+if(isset($_POST['search'])){
+    $searchTerm = $_POST['search'];
+    
+
+    foreach($allMovies as $movie){
+        $movieTitle = $movie->getTitle();
+        //this should check if $_POST['search'] is contained within the title of a movie
+        if (strpos($movieTitle, $searchTerm) == true) 
+        { 
+        $moviesList[] = $movie;
+        }
+    }
+}
+Page::render_movie_list($moviesList);
 
 // Page footer
 Page::footer();
