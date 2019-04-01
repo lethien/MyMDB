5d717765d072de131284cdf820d0130646cb6386
TrevorFawcett <49043544+TrevorFawcett@users.noreply.github.com>
2019-03-31 21:41:02 -0700

    Update MovieRestAPI.php

--- api/MovieRestAPI.php
+++ api/MovieRestAPI.php
@@ -12,7 +12,37 @@ parse_str(file_get_contents('php://input'), $requestData);
 
 switch($_SERVER['REQUEST_METHOD']) {
     case 'GET':
+        if (isset($requestData['title']))  {
+
+        $title = $requestData['title'];
+        $movie = MovieMapper::getMovie($title);
+
+        $jsonMovie = $movie->jsonSerialize();
         
+
+        header('Content-Type: application/json');
+        echo json_encode($jsonMovie);
+		}
+    else{
+        //Get all the movies
+        $movies = MovieMapper::getMovies();
+        //Initialize an array to hold the serialized movies
+        $serializedMovies = array();
+
+        //Go through all the movies and add them to the serialized array
+        foreach ($movies as $movie)   
+        {
+            $serializedMovies[] = $movie->jsonSerialize();
+        }
+
+        
+        //Set the header
+        header('Content-Type: application/json');
+        //Return all the movie!
+        echo json_encode($serializedMovies);
+    }
+    
+    break;
         
         break;
     case 'POST':
